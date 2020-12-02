30c88da47bfb5f7e50d7b32078b97546fd777ce7
ganjoe <ganjoe@yahoo.de>
2020-12-02 22:43:59 +0100

    545

--- hotwater_f407/Drivers/propelli/termfile.c
+++ hotwater_f407/Drivers/propelli/termfile.c
@@ -38,12 +38,25 @@ void init_cmdfile(TD_TERMFILE* cmd)
 	if (stat == FR_OK)
 	{
 		//TODO: unheil abwenden
-		cmd->cmdcounter=5;
+	cmd->cmdcounter=6;
 	cmdfile_do_cmds(&initcmd);
 	}
 
 }
 
+void 	cmdfile_do_cmds			(TD_TERMFILE* initcmd)
+	{
+	if (initcmd->flag_initdone == true)
+		{
+		for (int var = 0; var < initcmd->cmdcounter; ++var)
+			{
+			cmdfile_lol_readln(initcmd, initcmd->linebuffer, var);
+			//termfile_lol_parse(initcmd->linebuffer, initcmd->maxarguments);
+			memcpy(btTerm.string_rx, initcmd->linebuffer, strlen(initcmd->linebuffer));
+			term_lol_parse(&btTerm);
+			}
+		}
+	}
 
 FRESULT cmdfile_lol_open_create(TD_TERMFILE* initcmd)
 {
@@ -77,57 +90,8 @@ FRESULT cmdfile_lol_open_create(TD_TERMFILE* initcmd)
 	}
 	return stat;
 }
-/*
-https://c-for-dummies.com/blog/?p=1769
-The strsep() function is defined in the string.h header file. Here is the man page format:
-char * strsep(char **stringp, const char *delim);
 
-The first argument, **stringp, is one of those annoying pointer-pointer things;
-t’s the single string to search or an array of strings to search.
-The second argument, *delim, is a string of one or more separator characters,
-similar to the strtok() function.
-
-Like strtok(), the strsep() function returns a pointer to the first string yanked out of **strngp.
-Unlike strtok(), however, the function is called with the same arguments over and over until it returns a NULL pointer.
- */
-void cmdfile_do_cmds	(TD_TERMFILE* initcmd)
-	{
-	if (initcmd->flag_initdone == true)
-		{
-		for (int var = 0; var < initcmd->cmdcounter; ++var)
-			{
-			cmdfile_lol_readln(initcmd, initcmd->linebuffer, var);
-			//termfile_lol_parse(initcmd->linebuffer, initcmd->maxarguments);
-			memcpy(btTerm.string_rx, initcmd->linebuffer, strlen(initcmd->linebuffer));
-			term_lol_parse(&btTerm);
-			}
-		}
-	}
-static int callback_write = 0;
-void termfile_lol_parse(/*cmd parser typedef*/char* buffer, int maxArguments )
-    {
-    int argc = 0;
-    char *argv[maxArguments];
-    //cmd ist der erste stringabschnitt von links
-    char *p2 = strtok(buffer, " ");
-    //argumente separieren, und in ptr-array speichern
-    while (p2 && argc < maxArguments)
-		{
-		argv[argc++] = p2;
-		//sachen passieren
-		p2 = strtok(0," ");
-		}
-    for (int i = 0; i < callback_write; i++)
-		{
-    	if (callbacks[i].cbf != 0 && strcmp(argv[0], callbacks[i].command) == 0)
-	    	{
-    		callbacks[i].cbf(argc, (const char**) argv);
-    		return;
-	    	}
-		}
-    }
-
-FRESULT cmdfile_lol_readln(TD_TERMFILE* initcmd, char* buffer, int linenr)
+FRESULT cmdfile_lol_readln		(TD_TERMFILE* initcmd, char* buffer, int linenr)
 {
 	utils_truncate_number_int(&linenr, 0, initcmd->maxlines);
 	FRESULT stat;
@@ -138,7 +102,8 @@ FRESULT cmdfile_lol_readln(TD_TERMFILE* initcmd, char* buffer, int linenr)
 	stat = f_close(&SDFile);
 	return stat;
 }
-FRESULT cmdfile_lol_writeln(TD_TERMFILE* initcmd, char* buffer, int linenr)
+
+FRESULT cmdfile_lol_writeln		(TD_TERMFILE* initcmd, char* buffer, int linenr)
 {
 	FRESULT stat;
 	UINT byteswrote;
--- hotwater_f407/Drivers/propelli/termfile.h
+++ hotwater_f407/Drivers/propelli/termfile.h
@@ -9,11 +9,7 @@
 #define PROPELLI_TERMFILE_H_
 #include "datatypes.h"
 #include "fatfs.h"
-/*
- * empfängt und schreibt strings aus einer datei
- *  und schickt sie zum parser in terminal.c
- *
-*/
+
 typedef struct
 {
 	//zeilenbuffer/dateiname für schreiben/lesen
@@ -31,12 +27,15 @@ typedef struct
 }
 TD_TERMFILE;
 
-void init_cmdfile(TD_TERMFILE* initcmd);
-FRESULT cmdfile_lol_open_create(TD_TERMFILE* initcmd);
-void termfile_lol_parse(/*cmd parser typedef*/char* buffer, int maxArguments );
-void cmdfile_do_cmds	(TD_TERMFILE* initcmd);
-FRESULT cmdfile_lol_readln(TD_TERMFILE* initcmd, char* buffer, int linenr);
-FRESULT cmdfile_lol_writeln(TD_TERMFILE* initcmd, char* buffer, int linenr);
+void 	init_cmdfile			(TD_TERMFILE* initcmd);
+
+void 	cmdfile_do_cmds			(TD_TERMFILE* initcmd);
+
+FRESULT cmdfile_lol_open_create	(TD_TERMFILE* initcmd);
+
+FRESULT cmdfile_lol_readln		(TD_TERMFILE* initcmd, char* buffer, int linenr);
+
+FRESULT cmdfile_lol_writeln		(TD_TERMFILE* initcmd, char* buffer, int linenr);
 extern TD_TERMFILE initcmd;
 
 #endif /* PROPELLI_TERMFILE_H_ */
