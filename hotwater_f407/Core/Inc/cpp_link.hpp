65eb585e0ce8babd836cf4a5c516019d71d706f4
ganjoe <ganjoe@yahoo.de>
2020-12-19 19:45:07 +0100

    c von c++ callen

---
+++ hotwater_f407/Core/Inc/cpp_link.hpp
@@ -0,0 +1,22 @@
+/*
+ * cpp_link.hpp
+ *
+ *  Created on: 19.12.2020
+ *      Author: danie
+ */
+
+#ifndef INC_CPP_LINK_HPP_
+#define INC_CPP_LINK_HPP_
+
+    #ifdef __cplusplus
+    	extern "C"
+    	{
+    #endif
+    	void cpp_link (void);
+    #ifdef __cplusplus
+    	}
+    #endif
+
+
+
+#endif /* INC_CPP_LINK_HPP_ */
---
+++ hotwater_f407/Core/Src/cpp_link.cpp
@@ -0,0 +1,23 @@
+/**
+ * cpp_link.cpp
+ *
+ *  Created on: 19.12.2020
+ *      Author: daniel
+ *
+ * https://community.st.com/s/question/0D50X0000BFER0CSQX/cube32-feature-request-need-to-handle-cpp-files
+ */
+
+    #ifdef __cplusplus
+    	extern "C"
+    	{
+    #endif
+    	void cpp_link (void)
+    		{
+
+    		}
+    #ifdef __cplusplus
+    	}
+    #endif
+
+
+
